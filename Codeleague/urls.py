3558e7c5cc6c7328c53c14d84fbe1e7bdce7f7ce
andrei <andreisavin21@gmail.com>
2013-09-21 07:21:57 -0400

    fixed some bugs

--- Codeleague/urls.py
+++ Codeleague/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', 'main.views.index', name='index'),
+    url(r'^', include(main.views.index)),
     url(r'^admin/', include(admin.site.urls)),
 )
