411e1b68eb98b9cf582fa8055b3fc0f470b55d48
andrei <andreisavin21@gmail.com>
2013-09-21 07:58:44 -0400

    templates2

--- Codeleague/settings.py
+++ Codeleague/settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/andrei/Prog/hackathon/Codeleague/tpl',
+	os.path.join(PROJECT_ROOT, "tpl"),
 )
 
 INSTALLED_APPS = (
