a94f3fe2e6e0225161fe43775256dfa7e70c9b95
andrei <andreisavin21@gmail.com>
2013-09-21 06:59:14 -0400

    Bugs

--- Codeleague/urls.py
+++ Codeleague/urls.py
@@ -1,20 +1,9 @@
 from django.conf.urls import patterns, include, url
-from main import views
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Codeleague.views.home', name='home'),
-    # url(r'^Codeleague/', include('Codeleague.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-	
-url(r'^$', 'views.index', name='home'),
+    url(r'^', 'main.views.index', name='index'),
+    url(r'^admin/', include(admin.site.urls)),
 )
--- main/views.py
+++ main/views.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django_facebook.models import FacebookProfile
 
 def index(request):
-	if not request.user.is_authenticated():
-		return render_to_response('index.html', {'logged_in': 0}, context_instance=RequestContext(request))
-	else:
-		return render_to_response('index.html', {'logged_in': 1}, context_instance=RequestContext(request))
-
+	context = {'logged_in': 0}
+	return render(request, 'index.html', context)
