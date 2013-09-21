b252891878d616151edd1945279fd6d679af70bf
andrei <andreisavin21@gmail.com>
2013-09-21 07:35:37 -0400

    views

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
 	context = {'logged_in': 0}
-	return render(request, 'index.html', context)
+	return render(request, 'templates/index.html', context)
