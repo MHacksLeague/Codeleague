f2f044c1bcc48dcb19e07662a757909a4b3b374b
andrei <andreisavin21@gmail.com>
2013-09-21 07:53:04 -0400

    templates2

--- Codeleague/settings.py
+++ Codeleague/settings.py
@@ -60,7 +60,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/bootstrap/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/andrei/Prog/hackathon/Codeleague/templates',
+	'/home/andrei/Prog/hackathon/Codeleague/tpl',
 )
 
 INSTALLED_APPS = (
