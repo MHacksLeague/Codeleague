945f0194c0863f990655194c94f65bffdfe0c541
andrei <andreisavin21@gmail.com>
2013-09-21 17:29:25 -0400

    #5

--- Codeleague/urls.py
+++ Codeleague/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/andrei/Prog/hackathon/Codeleague/static'}),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/static'}),
 	url(r'^profile/?', 'rank.views.HackerProfile'),
     url(r'^ranking/?', 'rank.views.Ranking'),
     url(r'^', 'main.views.index'),
