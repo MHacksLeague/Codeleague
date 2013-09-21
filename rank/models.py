ef1713f86bf20b14501416a52e2190a763472e19
davinwong <davinwong@outlook.com>
2013-09-21 12:50:10 -0400

    started rank app for profile/ranking models

---
+++ rank/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+class UserProfile(models.Model):
+	user = models.ForeignKey('User')
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	school = models.CharField(max_length=30)
+	github = models.CharField(max_length=30)
+	# rank = property(get_rank)
+	score = property(get_score)
+
+	def get_score:
+		score = 0
+
+		results = Result.objects.filter(user_profile=self.id)
+
+		for result in results:
+			score += result.points
+
+		return score
+	# def get_rank:
+
+
+class Hackathon(models.Model):
+	name = models.CharField(max_length=30)
+
+class Result(models.Model):
+	user_profile = models.ForeignKey('UserProfile')
+	hackathon = models.ForeignKey('Hackathon')
+	rank = models.IntegerField()
+	points = models.IntegerField()
